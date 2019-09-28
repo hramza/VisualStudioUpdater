73bc6bf6cc05121d4e4c92e87b4024d8e3e57157
Hamza RAMZA <ramza.hamza.dev@gmail.com>
2019-09-28 23:06:35 +0200

    Generate the package and install it in client machine

---
+++ InstallPackageTool.ps1
@@ -0,0 +1,5 @@
+dotnet pack
+dotnet tool install --global --add-source .\VisualStudioUpdater\nupkg VisualStudioUpdater
+
+# To delete, run the bellow command:
+# dotnet tool uninstall --global VisualStudioUpdater
